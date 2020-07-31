f50af293f6b31aafd27de5a3effaedb5d24c1b7d
Md Arman Ullah <65160577+mdarmancse@users.noreply.github.com>
2020-07-31 16:39:45 +0600

    Getting Started
    Getting Started

---
+++ src/components/TopBanner/TopBanner.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {Button, Col, Container, Row} from "react-bootstrap";
+import '../../asset/css/custom.css';
+
+class TopBanner extends Component {
+    render() {
+        return (
+            <>
+                <Container fluid={true} className="topFixedBanner p-0">
+                    <div className="topBannerOverlay">
+                        <Container className="topContent">
+                            <Row>
+                                <Col className="text-center">
+                                    <h1 className='topTitle'>Software Engineer</h1>
+                                    <h4 className="topSubtitle">Mobile & Web Application</h4>
+                                    <Button variant="primary">More Info</Button>
+
+
+                                </Col>
+
+
+                            </Row>
+
+
+                        </Container>
+
+
+                    </div>
+
+
+
+                </Container>
+
+            </>
+        );
+    }
+}
+
+export default TopBanner;
---
+++ src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import blackLogo from "../../asset/images/black.png";
+import blueLogo from "../../asset/images/blue.png";
+
+class TopNavigation extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            navBarTitle:"navTitle",
+            navLogo:[blackLogo],
+            navBG:"navBackground",
+            navBarItem:"navItem"
+        }
+    }
+
+    onScroll=()=>{
+      if(window.scrollY>100){
+            this.setState({navBarTitle:"navTitleScroll",navLogo:[blueLogo],navBG:"navBackgroundScroll",navBarItem:"navItemScroll"})
+      }
+      else if(window.scrollX<100){
+          this.setState({navBarTitle:"navTitle",navLogo:[blackLogo],navBG:"navBackground",navBarItem:"navItem"})
+
+      }
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll',this.onScroll)
+    }
+
+    render() {
+        return (
+            <>
+                <Navbar className={this.state.navBG} fixed="top" collapseOnSelect expand="lg"  variant="dark">
+                    <Navbar.Brand className={this.state.navBarTitle}><img src={this.state.navLogo}/> aRman</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+                        <Nav className="ml-auto">
+                            <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+                            <Nav.Link  className={this.state.navBarItem} href="#deets">SERVICES</Nav.Link>
+                            <Nav.Link  className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+                            <Nav.Link  className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+                            <Nav.Link  className={this.state.navBarItem} href="#deets">CONTACT</Nav.Link>
+                            <Nav.Link  className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+
+            </>
+        );
+    }
+}
+
+export default TopNavigation;
