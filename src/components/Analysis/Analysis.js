3e99eae9a6c55ad83a7c73aa7ebb40f9f1dc603f
Md Arman Ullah <65160577+mdarmancse@users.noreply.github.com>
2020-07-31 20:48:24 +0600

    Update Analysis.js

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -9,14 +9,14 @@ class Analysis extends Component {
         this.state={
             data:[
 
-                {Technology:"Java",Projects:"100"},
-                {Technology:"React",Projects:"95"},
-                {Technology:"Kotlin",Projects:"20"},
-                {Technology:"PHP",Projects:"30"},
-                {Technology:"Django",Projects:"80"},
-                {Technology:"Python",Projects:"40"},
-                {Technology:"Angular",Projects:"60"},
-                {Technology:"Laravel",Projects:"70"}
+                {Technology:"Java",Projects:100},
+                {Technology:"React",Projects:95},
+                {Technology:"Kotlin",Projects:20},
+                {Technology:"PHP",Projects:30},
+                {Technology:"Django",Projects:80},
+                {Technology:"Python",Projects:40},
+                {Technology:"Angular",Projects:60},
+                {Technology:"Laravel",Projects:70}
 
 
 
@@ -32,18 +32,7 @@ class Analysis extends Component {
                 <Container classname="text-center">
                     <h1 className="serviceMainTitle text-center">Technology Used</h1>
                     <Row>
-                        <Col lg={6} sm={12} md={12} style={{width:"100%",height:"300px"}}>
-                                <ResponsiveContainer>
-                            <BarChart width={100} height={300} data={this.state.data}>
-                                <XAxis dataKey="Technology"/>
-                                <Tooltip />
-                                <Bar datakey="Projects" fill={blue}>
-                                </Bar>
-                            </BarChart>
-                                </ResponsiveContainer>
 
-
-                        </Col>
 
                         <Col lg={6} sm={12} md={12}>
                             <p className="serviceDes text-justify">
@@ -53,6 +42,18 @@ class Analysis extends Component {
 
                                 According to client demand I use PHP OOP, CodeIgniter and Laravel to build admin panel section. Application security is a big deal for commercial application.I always ensure security portion of my application, moreover i build a security alert system, to notify admin when his system at risk.
                             </p>
+
+                        </Col>
+                        <Col lg={6} sm={12} md={12} style={{width:"100%",height:"300px"}}>
+                            <ResponsiveContainer>
+                                <BarChart width={100} height={300} data={this.state.data}>
+                                    <XAxis dataKey="Technology"/>
+                                    <Tooltip />
+                                    <Bar datakey="Projects" fill={blue}>
+                                    </Bar>
+                                </BarChart>
+                            </ResponsiveContainer>
+
 
                         </Col>
 
